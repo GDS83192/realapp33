66e90219dc4bf839301e11828c29b2ea79ed4f52
Graham <Admin@mitchells-MBP.home>
2018-01-24 07:16:39 -0500

    db

---
+++ db.php
@@ -0,0 +1,4 @@
+<?php
+    $conn = mysql_connect('mysql://b2512d5aee49bb:21be0daa@us-cdbr-iron-east-05.cleardb.net/heroku_aef884d8da87272?reconnect=true','b2512d5aee49bb','21be0daa');
+    $db= mysql_select_db('visitors');
+?>
