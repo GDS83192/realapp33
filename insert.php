594abee774dbb8829a916710e8d21d3a10fec5cc
GDS83192 <graham.squires3@gmail.com>
2018-01-26 11:58:29 -0500

    more fix

--- insert.php
+++ insert.php
@@ -3,14 +3,14 @@
   $con = mysqli_connect('gmgcjwawatv599gq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 
   if(!$con)
-  (
+  {
     echo 'Not Connected to Server';
-  )
+  }
 
   if(!mysqli_select_db($con, 'aobcpskiubl03ixi'))
-  (
+  {
     echo 'Database NOt Selected';
-  )
+  }
 
   $Name = $_POST['username'];
   $Email = $_POST['email'];
@@ -18,13 +18,13 @@
   $sql = "INSERT INTO person (Name,Email) VALUES ('$Name', '$Email')";
 
   if(!mysqli_query($con,$sql))
-  (
+  {
     echo 'Not Inserted';
-  )
+  }
   else
-  (
+  {
     echo 'Inserted';
-  )
+  }
 
     header("refresh:2; url=index.html");
 ?>
