28b9c14142b10402bc1869e09176cdc9cd29f4df
GDS83192 <graham.squires3@gmail.com>
2018-02-04 11:44:49 -0500

    making js work

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 function revealMessage(){
   document.getElementbyId("hiddenMessage").style.display = "block";
 }
+
+function countDown() {
+  var currentVal = document.getElementbyId("countDownButton").innerHTML;
+  var newVal = 0;
+  if (currentVal > 0){
+    newVal = currenVal - 1;
+  }
+
+  document.getElementById("countDownButton").innerHTML = newVal;
+}
